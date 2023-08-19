2162b953ac69f19e2170f1448d57b7d0fd7c73d3
lunarmoon7 <49crehbgr@gmail.com>
2023-08-19 23:59:06 +0900

    fix: window.Kakao 에러 수정

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -1,18 +1,20 @@
 'use client';
 
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {
+  GoogleAuthProvider,
+  OAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
-import { auth } from '@/app/config/firebase';
+import { auth } from '@/config/firebase';
 import { RiKakaoTalkFill } from '@react-icons/all-files/ri/RiKakaoTalkFill';
 import { FcGoogle } from '@react-icons/all-files/fc/FcGoogle';
 
-
 import LoginDialog from './Dialog';
 import { usePathname, useRouter } from 'next/navigation';
-// import kakaoTalkLogo from '/public/kakaotalk_logo.svg';
-// import googleLogo from '/public/google_logo.svg';
 
 type Props = {};
+
 
 declare global {
   interface Window {
@@ -20,7 +22,7 @@ declare global {
   }
 }
 
-const redirectUri = `${window.location.origin}/kakaotalk`;
+const redirectUri = `http://localhost:3000/oauth/callback/kakao`;
 const scope = [
   'profile_nickname',
   'profile_image',
@@ -33,9 +35,29 @@ const scope = [
 ].join(',');
 
 export default function Login({}: Props) {
+  useEffect(() => {
+    console.log('window.Kakao: ', window.Kakao);
+    if (window.Kakao) {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+        console.log('after Init: ', window.Kakao.isInitialized());
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (window.Kakao) {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+        console.log('after Init: ', window.Kakao.isInitialized());
+      }
+    }
+  }, [window.Kakao]);
+  
   const router = useRouter();
   // 카카로 로그인 Handler
   const [isOpen, setIsOpen] = useState(false);
+  const [accessToken, setAccessToken] = useState<string>('');
 
   const openDialog = () => {
     setIsOpen((prev) => !prev);
@@ -44,13 +66,54 @@ export default function Login({}: Props) {
     setIsOpen((prev) => !prev);
   };
 
+  const firebaseLogin = (email: string) => {
+    const provider = new OAuthProvider('oidc.kakao');
+    // signInWithRedirect(auth, provider);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        const credential = OAuthProvider.credentialFromResult(result);
+        const accessToken = credential?.accessToken;
+        const idToken = credential?.idToken;
+        // console.log('Firebase AccessToken', accessToken);
+        // console.log('Firebase IdToken', idToken);
+        // getKakaoUserInfo(accessToken ? accessToken : '');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    router.push('/home');
+  };
+
+  // useEffect(() => {
+  //   async function myFunction() {
+  //     const csrfToken = await getCsrfToken();
+  //     console.log(csrfToken);
+  //     getKakaoUserInfo(csrfToken ? csrfToken : '');
+  //     setAccessToken(csrfToken ? csrfToken : '');
+  //   }
+
+  //   if (session?.user) {
+  //     const userInfo = session.user;
+  //     console.log('Session ', userInfo);
+  //     myFunction();
+  //     firebaseLogin(userInfo.email ? userInfo.email : '');
+  //   }
+  // }, [session]);
+
   const kakaoLoginHandler = () => {
+    // signIn('kakao');
+    // if(!Kakao?.isInitialized()) {
+    //   console.log(Kakao.isInitialized())
+    //   Kakao?.init(process.env.NEXT_PUBLIC_KAKAO_REST_KEY);
+    // }
     window.Kakao.Auth.authorize({
       redirectUri,
       scope,
     });
     console.log('Kakao Logining');
   };
+
   // 구글 로그인 Handler
   const googleLoginHandler = async () => {
     console.log('Google Logining');
@@ -65,16 +128,7 @@ export default function Login({}: Props) {
       });
   };
 
-  useEffect(() => {
-    function kakaoInit() {
-      if (!window.Kakao?.isInitialized()) {
-        window.Kakao?.init(process.env.NEXT_PUBLIC_KAKAO_REST_KEY);
-        console.log(window.Kakao?.isInitialized());
-      }
-    }
-
-    kakaoInit();
-  }, []);
+  
 
   return (
     <div className='w-full flex flex-col justify-center items-center space-y-6 mb-20'>
