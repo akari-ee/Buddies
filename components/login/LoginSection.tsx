f8cee87b92ce8f8b8db0ff7a3f51aa8885c31d4b
lunarmoon7 <49crehbgr@gmail.com>
2023-08-21 15:40:48 +0900

    fix: 카카오 SDK 추가 안되는 버그 수정 - 수동으로 SDK script 추가

--- components/login/LoginSection.tsx
+++ components/login/LoginSection.tsx
@@ -8,22 +8,36 @@ import service_title from '/public/service_title_black.svg';
 import bomi from '/public/bomi.svg';
 import BackgroundCircles from './BackgroundCircles';
 import CharacterCarousel from './CharacterCarousel';
-import LogoHeader from '../UI/LogoHeader';
+import Logo from '../UI/Logo';
 
 type Props = {};
 
-export default function LoginSection({}: Props) {
-  // const { user } = useAuth();
+import { useUser, withUser, withUserTokenSSR } from 'next-firebase-auth';
 
-  // useEffect(() => {
-  //   console.log(user);
-  // }, []);
+export default function LoginSection({}: Props) {
+  useEffect(() => {
+    const kakaoSDK = document.createElement('script');
+    kakaoSDK.async = false;
+    kakaoSDK.src = `https://t1.kakaocdn.net/kakao_js_sdk/2.3.0/kakao.min.js`;
+    kakaoSDK.integrity = `sha384-70k0rrouSYPWJt7q9rSTKpiTfX6USlMYjZUtr1Du+9o4cGvhPAWxngdtVZDdErlh`;
+    kakaoSDK.crossOrigin = `anonymous`;
+    document.head.appendChild(kakaoSDK);
+
+    const onLoadKakaoAPI = () => {
+      if (!window.Kakao.isInitialized()) {
+        window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+        console.log('after Init: ', window.Kakao.isInitialized());
+      }
+    };
+
+    kakaoSDK.addEventListener('load', onLoadKakaoAPI);
+  }, []);
 
   return (
     <div className='w-screen h-screen relative flex flex-col justify-between overflow-hidden'>
       <div className='flex flex-col items-start'>
         <div className='flex flex-col items-start justify-between gap-8 pt-5 pl-6 md:gap-12'>
-          <LogoHeader serviceTitle={service_title} isHome={false} />
+          <Logo serviceTitle={service_title} isHome={false} />
           <div className='flex flex-col items-start gap-6 md:gap-8'>
             <div className='font-PyeongChangPeace text-3xl md:text-6xl'>
               <p className='font-light'>Find your</p>
