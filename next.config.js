d80f0bf127f5e9d6caad907903ed46becbd9ce40
lunarmoon7 <49crehbgr@gmail.com>
2023-08-23 18:42:40 +0900

    build: 프로필 이미지 등록을 위한 도메인 수정 - next/image 사용을 위해 필수 - domain 등록 안할 시 에러 발생

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com', 'k.kakaocdn.net'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
