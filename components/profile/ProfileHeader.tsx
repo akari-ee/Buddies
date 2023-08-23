ab662f48b718c0ff8165ca65d1f1eb73805f5a53
lunarmoon7 <49crehbgr@gmail.com>
2023-08-23 17:24:39 +0900

    refactor: router ➡️ setOpen - 페이지 이동이 아닌, slide-over로

--- components/profile/ProfileHeader.tsx
+++ components/profile/ProfileHeader.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { useRouter } from 'next/navigation';
 import { BiArrowBack } from '@react-icons/all-files/bi/BiArrowBack';
 
-type Props = {}
-
-export default function ProfileHeader({}: Props) {
+export default function ProfileHeader({ setOpen }: ProfileHeaderProps) {
   const router = useRouter();
   return (
     <div>
-      <button onClick={() => router.back()}>
-        <BiArrowBack size={30} color="black" />
+      <button onClick={setOpen}>
+        <BiArrowBack size={30} color='black' />
         {/* <Image src={prevBtn} alt='go to prev' width={21} height={17}/> */}
       </button>
     </div>
-  )
+  );
 }
--- components/profile/ProfileSection.tsx
+++ components/profile/ProfileSection.tsx
@@ -1,16 +1,14 @@
 'use client';
 
-import React from 'react'
+import React from 'react';
 import ProfileHeader from './ProfileHeader';
 import ProfileInfo from './ProfileInfo';
 
-type Props = {}
-
-export default function ProfileSection({}: Props) {
+export default function ProfileSection({ setOpen }: ProfileSectionProps) {
   return (
     <div className='w-full h-full flex flex-col gap-10'>
-      <ProfileHeader />
+      <ProfileHeader setOpen={setOpen} />
       <ProfileInfo />
     </div>
-  )
+  );
 }
