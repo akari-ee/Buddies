616a8efd93a6409ca4309aceb86ccc51dd568db5
lunarmoon7 <49crehbgr@gmail.com>
2023-08-25 22:47:38 +0900

    feat: 메시지에 타임스탬프 추가

--- components/chat/ChatBox.tsx
+++ components/chat/ChatBox.tsx
@@ -1,20 +1,31 @@
 import { cn } from '@/utils/extendClass';
-import React from 'react'
+import React from 'react';
 
 const bg_colors = ['bg-bomi', 'bg-yermi', 'bg-gauri', 'bg-gyeouri'];
 
-export default function ChatBox({styles, message, characterId}: ChatBox) {
+export default function ChatBox({
+  styles,
+  message,
+  characterId,
+  timestamp,
+}: ChatBox) {
   return (
-    <div className={cn('flex flex-col mb-2 w-full', styles.align)}>
-      <div className={cn('flex max-w-[calc(45%)]', characterId === -1 ? styles.bg: bg_colors[characterId], styles.color, styles.radius)}>
+    <div className={cn('flex items-end mb-2 gap-2', styles.align, characterId === -1 ? 'flex-row-reverse': '')}>
+      <div className='text-xs text-gray-600'>{timestamp}</div>
+      <div
+        className={cn(
+          'flex max-w-[calc(45%)]',
+          characterId === -1 ? styles.bg : bg_colors[characterId],
+          styles.color,
+          styles.radius
+        )}
+      >
         <div className='px-2 py-3 text-md'>
-          <span>
-            {message}
-          </span>
+          <span>{message}</span>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 // padding='8px 14px'
