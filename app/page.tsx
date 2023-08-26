69c56b7216ad42498e2917a5821cd5a8b6d5f1c3
lunarmoon7 <49crehbgr@gmail.com>
2023-08-26 15:51:15 +0900

    feat: Splash 로직 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
+'use client';
+
 import LoginSection from '@/components/login/LoginSection';
 import Splash from '@/components/UI/Splash';
-import { auth } from '@/config/firebase';
-import { cookies } from 'next/headers';
-export default async function Home() {
-  return (
+import { useEffect, useState } from 'react';
+export default function Home() {
+  const [showSplash, setShowSplash] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 3000);
+    return () => clearTimeout(timer); // 컴포넌트가 언마운트되면 타이머를 제거
+  }, []);
+  return showSplash ? (
+    <Splash />
+  ) : (
     <div className='w-screen h-screen flex flex-col items-start shrink-0 min-w-full'>
       <section id='login_section'>
         <LoginSection />
-        {/* <Splash /> */}
       </section>
     </div>
   );
