50a649e9d918aeea8b27d06f8fa2a8164154a945
lunarmoon7 <49crehbgr@gmail.com>
2023-08-30 19:32:41 +0900

    fix: jwt 저장을 위한 interface 확장

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,15 +1,24 @@
-// //types/next-auth.d.ts
+//types/next-auth.d.ts
 
-// import NextAuth from "next-auth";
+import NextAuth from 'next-auth';
 
-// declare module "next-auth" {
-//   /**
-//    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-//    */
-//   interface Session {
-//     user: {
-//       id: any;
-//       email: string;
-//     };
-//   }
-// }
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session extends DefaultSession {
+    user?: {
+      id?: string;
+    } & DefaultSession['user'];
+    accessToken: string;
+    provider: string;
+  }
+  interface DefaultJWT {
+    name?: string | null;
+    email?: string | null;
+    picture?: string | null;
+    sub?: string;
+    accessToken?: string;
+    provider?: string | null;
+  }
+}
