95df76b6eacbde8366fffb2ea27fb5e395c4d92d
lunarmoon7 <49crehbgr@gmail.com>
2023-08-30 21:57:16 +0900

    feat: getToken() 추가

--- middleware.ts
+++ middleware.ts
@@ -11,18 +11,14 @@ import { getToken } from 'next-auth/jwt';
 const PUBLIC_PATHS = ['/home', '/chat', '/user', '/user/profile', '/chat/0'];
 
 export async function middleware(req: NextRequest) {
-  // const session = await getServerSession(req as any, response as any, authOptions);
-  // console.log(session);
-  // const session = await getSession( {req: request} );
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  // console.log(req.nextauth.token);
-  console.log("token is: ", token);
+  // getSession
+  // const session = await getSession({ req });
+  // console.log('middleware session is: ', session);
+
+  // Authentication using Cookie  
   if (req.cookies.has('uid')) {
     // 로그인 했다면 (쿠키가 있다면)
-    if (
-      req.nextUrl.pathname === '/' ||
-      req.nextUrl.pathname === '/login'
-    ) {
+    if (req.nextUrl.pathname === '/' || req.nextUrl.pathname === '/login') {
       return NextResponse.redirect(new URL('/home', req.url)); // 홈으로 이동
     }
   } else {
@@ -31,4 +27,21 @@ export async function middleware(req: NextRequest) {
       return NextResponse.redirect(new URL('/login', req.url)); // 로그인 페이지로 이동
     }
   }
+
+  // getToken
+  // const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  // console.log("token is: ", token);
+
+  // // Authentication using Token
+  // if (token) {
+  //   // 로그인 했다면 (쿠키가 있다면)
+  //   if (req.nextUrl.pathname === '/' || req.nextUrl.pathname === '/login') {
+  //     return NextResponse.redirect(new URL('/home', req.url)); // 홈으로 이동
+  //   }
+  // } else {
+  //   // 로그인하지 않았다면 (쿠키가 없다면)
+  //   if (PUBLIC_PATHS.includes(req.nextUrl.pathname)) {
+  //     return NextResponse.redirect(new URL('/login', req.url)); // 로그인 페이지로 이동
+  //   }
+  // }
 }
