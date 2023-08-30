e7fb81645360928d8e133669356ef92f3a2d6c06
lunarmoon7 <49crehbgr@gmail.com>
2023-08-30 18:07:25 +0900

    feat: Adapter 추가

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,18 @@
+import { firestore } from '@/config/firebase';
+import { FirestoreAdapter } from '@auth/firebase-adapter';
 import NextAuth from 'next-auth';
+import { Adapter } from 'next-auth/adapters';
 import KakaoProvider from 'next-auth/providers/kakao';
 
 const authOptions = NextAuth({
+  adapter: FirestoreAdapter(firestore) as Adapter,
   providers: [
     KakaoProvider({
       clientId: process.env.NEXT_PUBLIC_KAKAO_REST_KEY!,
       clientSecret: process.env.NEXT_PUBLIC_KAKAO_SECRET_KEY!,
     }),
   ],
+  callbacks: {},
 });
 
 export { authOptions as GET, authOptions as POST, authOptions };
