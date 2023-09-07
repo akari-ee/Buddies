7e203a0bbdf800da9dd74a797767c9af4d4de1c1
lunarmoon7 <49crehbgr@gmail.com>
2023-09-07 21:05:47 +0900

    bug: replace() error

--- config/firebase.ts
+++ config/firebase.ts
@@ -20,7 +20,7 @@ const firebaseConfig = {
 };
 
 const firebaseAdminConfig = {
-  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n') as string,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') as string,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   projectId: process.env.FIREBASE_PROJECT_ID,
 };
