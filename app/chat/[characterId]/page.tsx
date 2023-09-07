bd4f7bd48f3153f3d9bd4401e0450e98a200b58c
lunarmoon7 <49crehbgr@gmail.com>
2023-09-08 00:40:38 +0900

    fix: 비회원 시 user 접근 못하는 에러 수정

--- app/chat/[characterId]/page.tsx
+++ app/chat/[characterId]/page.tsx
@@ -26,23 +26,35 @@ export default async function Chat({
 async function getChatList(characterId: string) {
   const session: unknown = await getServerSession(authOptions);
   // unknown 에러 방지
+  if (
+    (session as { user: { name: string; email: string; image: string } })
+      .user === undefined ||
+    (session as { user: { name: string; email: string; image: string } })
+      .user === null
+  ) {
+    return [];
+  }
+  
   const email = (
     session as { user: { name: string; email: string; image: string } }
   ).user.email;
 
-  // 익명(비회원) 사용자는 챗봇 대화기록을 저장하지 않는다.
-  if (email === undefined || email === null || email.length === 0) {
-    return [];
-  }
+  // // 익명(비회원) 사용자는 챗봇 대화기록을 저장하지 않는다.
+  // if (email === undefined || email === null || email.length === 0) {
+  //   return [];
+  // }
 
-  const res = await fetch('https://buddies-next-js.vercel.app/api/firebase/getChatList', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email: email, characterId: characterId }),
-    // next: { revalidate: 5 },
-  });
+  const res = await fetch(
+    'https://buddies-next-js.vercel.app/api/firebase/getChatList',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: email, characterId: characterId }),
+      // next: { revalidate: 5 },
+    }
+  );
 
   const data = await res.json();
   const initialMessages = handleChatList(data.data);
