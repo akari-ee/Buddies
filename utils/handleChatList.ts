db49b32b97034791fedbcff06a91892486cf39d5
lunarmoon7 <49crehbgr@gmail.com>
2023-09-10 16:42:58 +0900

    refactor: 메시지가 유실된 경우 예외처리

--- utils/handleChatList.ts
+++ utils/handleChatList.ts
@@ -3,22 +3,24 @@ import { randomBytes, randomUUID } from 'crypto';
 import { Message } from 'ai';
 
 export const handleChatList = (chatList: any[]) => {
-  let initialMessages : Message[] = [];
+  let initialMessages: Message[] = [];
   chatList.forEach((chat) => {
-    for (let [index, el] of chat.user.entries()) {
-      initialMessages.push({
-        id: randomBytes(16).toString('hex'),
-        role: el.role,
-        content: el.content,
-        createdAt: new Date(el.timestamp),
-      });
-      if (index < chat.gpt.length) {
+    if (chat.user) {
+      for (let [index, el] of chat.user.entries()) {
         initialMessages.push({
           id: randomBytes(16).toString('hex'),
-          role: chat.gpt[index].role,
-          content: chat.gpt[index].content,
-          createdAt: new Date(chat.gpt[index].timestamp),
+          role: el.role,
+          content: el.content,
+          createdAt: new Date(el.timestamp),
         });
+        if (index < chat.gpt.length) {
+          initialMessages.push({
+            id: randomBytes(16).toString('hex'),
+            role: chat.gpt[index].role,
+            content: chat.gpt[index].content,
+            createdAt: new Date(chat.gpt[index].timestamp),
+          });
+        }
       }
     }
   });
