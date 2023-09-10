0593f81b4164391eb22db18a46afa7172bfcd5d1
lunarmoon7 <49crehbgr@gmail.com>
2023-09-10 14:58:09 +0900

    build: config 수정

--- next.config.js
+++ next.config.js
@@ -3,12 +3,22 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   maximumFileSizeToCacheInBytes: 10000000,
 });
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'k.kakaocdn.net'],
+    domains: ['lh3.googleusercontent.com', 'k.kakaocdn.net', 'localhost'],
   },
 };
 
-module.exports = withPWA({ nextConfig });
+module.exports = process.env.NODE_ENV === 'development' ? nextConfig : withPWA({ nextConfig });
+
+// module.exports = process.env.NODE_ENV === 'development' ? {
+//   images: {
+//     domains: ['lh3.googleusercontent.com', 'k.kakaocdn.net', 'localhost'],
+//   },
+// } : withPWA({
+//   images: {
+//     domains: ['lh3.googleusercontent.com', 'k.kakaocdn.net', 'localhost'],
+//   },
+// });
