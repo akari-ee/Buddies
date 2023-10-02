e2ea011365672442489db1ca914ac21d578d0c8a
lunarmoon7 <49crehbgr@gmail.com>
2023-10-02 16:37:02 +0900

    feat: CSR로 변경

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,15 +1,94 @@
-import UserSection from '@/components/user/UserSection'
-import React from 'react'
+'use client';
 
-type Props = {}
+import UserSection from '@/components/user/UserSection';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
+import React, { useCallback, useEffect, useState } from 'react';
+import { promptState } from '@/store/atoms';
+import { cn } from '@/utils/extendClass';
+import { useRecoilValue } from 'recoil';
+import { getSession } from 'next-auth/react';
 
-export default function User({}: Props) {
+type Props = {};
+const bg_colors = ['bg-bomi', 'bg-yermi', 'bg-gauri', 'bg-gyeouri'];
+
+export default async function User({}: Props) {
+  const [chatUsageData, setChatUsageData] = useState({
+    '보미': 0,
+    '여르미': 0,
+    '가으리': 0,
+    '겨우리': 0,
+  });
   // bg-gauri bg-gradient-to-b  from-gauri via-gauri/20 to-white
+  // const chatUsageData = await getChatUsage();
+  const value = useRecoilValue(promptState);
+  const getChatUsage = useCallback(async () => {
+    const session: unknown = await getSession(authOptions);
+    // 익명(비회원) 사용자는 챗봇 대화기록을 저장하지 않는다.
+    if (session === null || session === undefined) {
+      return [];
+    }
+
+    // unknown 에러 방지
+    const email = (
+      session as { user: { name: string; email: string; image: string } }
+    ).user.email;
+
+    const chatUsageResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/firebase/getChatUsage`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email }),
+      }
+    );
+
+    const chatUsageData = await chatUsageResponse.json();
+    setChatUsageData(chatUsageData.data.chatUsage);
+  }, []);
+
+  useEffect(() => {
+    getChatUsage();
+  }, [])
   return (
-    <div className='w-screen h-screen flex flex-col items-start shrink-0 min-w-full bg-gray-400'>
+    <div
+      className={cn(
+        'w-screen h-screen flex flex-col items-start shrink-0 min-w-full', bg_colors[value]
+      )}
+    >
       <section id='login_section' className=''>
-        <UserSection />
+        <UserSection chatUsageData={chatUsageData} />
       </section>
     </div>
-  )
+  );
 }
+
+// async function getChatUsage() {
+//   const session: unknown = await getServerSession(authOptions);
+//   // 익명(비회원) 사용자는 챗봇 대화기록을 저장하지 않는다.
+//   if (session === null || session === undefined) {
+//     return [];
+//   }
+
+//   // unknown 에러 방지
+//   const email = (
+//     session as { user: { name: string; email: string; image: string } }
+//   ).user.email;
+
+//   const chatUsageResponse = await fetch(
+//     `${process.env.NEXT_PUBLIC_BASE_URL}/api/firebase/getChatUsage`,
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ email: email }),
+//     }
+//   );
+
+//   const chatUsageData = await chatUsageResponse.json();
+
+//   return chatUsageData.data.chatUsage;
+// }
