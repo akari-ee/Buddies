16b8adef46a977541da719104d613b260d4ec786
lunarmoon7 <49crehbgr@gmail.com>
2023-10-22 16:43:03 +0900

    build: opanAI config 수정

--- config/chatgpt.ts
+++ config/chatgpt.ts
@@ -1,9 +1,22 @@
 import { Configuration, OpenAIApi } from 'openai';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import {
+  ChatPromptTemplate,
+  SystemMessagePromptTemplate,
+  MessagesPlaceholder,
+  HumanMessagePromptTemplate,
+  PromptTemplate,
+  FewShotPromptTemplate
+} from 'langchain/prompts';
 
 const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-})
+});
 
 const openai = new OpenAIApi(configuration);
+const chatModel = new ChatOpenAI({
+  openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
+
 
 export default openai;
