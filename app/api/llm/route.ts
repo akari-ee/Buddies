8f72843f54ebf0a08d2599aba407cb363251545e
lunarmoon7 <49crehbgr@gmail.com>
2023-11-12 21:39:12 +0900

    refactor: remove unused code - remove modules, functions

--- app/api/llm/route.ts
+++ app/api/llm/route.ts
@@ -1,28 +1,10 @@
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import {
-  ChatMessageHistory,
-  ConversationSummaryBufferMemory,
-} from 'langchain/memory';
-import { ConversationChain, LLMChain } from 'langchain/chains';
-import { CallbackManager } from 'langchain/callbacks';
-import {
-  promptSpring,
-  promptSummer,
-  promptAutumn,
-  promptWinter,
-} from '@/config/prompts';
-import { NextResponse } from 'next/server';
 import {
   Message as VercelChatMessage,
   StreamingTextResponse,
   LangChainStream,
-  OpenAIStream,
 } from 'ai';
-import { BytesOutputParser } from 'langchain/schema/output_parser';
-import { PromptTemplate } from 'langchain/prompts';
-import { chatModel, llms } from '@/config/langchain';
-import dayjs from 'dayjs';
-import { AIMessage, ChainValues, HumanMessage } from 'langchain/schema';
+import { llms } from '@/config/langchain';
+import { AIMessage, HumanMessage } from 'langchain/schema';
 import {
   saveChatHistoryInToFirebaseDatabase,
   saveCompletionInToFirebaseDatabase,
@@ -36,13 +18,8 @@ const characters = ['spring', 'summer', 'autumn', 'winter'];
  * Basic memory formatter that stringifies and passes
  * message history directly into the model.
  */
-const formatMessage = (message: VercelChatMessage) => {
-  return `${message.role}: ${message.content}`;
-};
-
 export async function POST(request: Request) {
-  const { email, characterId, messages, initialMessages } =
-    await request.json();
+  const { email, characterId, messages } = await request.json();
   const currentMessageContent = messages[messages.length - 1].content;
   const characterName = characters[characterId];
 
@@ -51,26 +28,23 @@ export async function POST(request: Request) {
       await saveChatHistoryInToFirebaseDatabase(email, '가으리', messages);
     },
     onCompletion: async (completion: string) => {
-      console.log('onCompletion: ', completion);
       await saveCompletionInToFirebaseDatabase(email, '가으리', completion);
     },
   });
 
-  llms[characterName].run(currentMessageContent, [handlers]);
+  // Must be called without await
+  // input object must be passed first
+  llms[characterName].call(
+    {
+      input: currentMessageContent,
+    },
+    (messages as Message[]).map((m: any) =>
+      m.role === 'assistant'
+        ? new AIMessage(m.content)
+        : new HumanMessage(m.content)
+    ),
+    [handlers]
+  );
 
-  // must call without await
-  llms[characterName]
-    .call(
-      {
-        input: currentMessageContent,
-      },
-      (messages as Message[]).map((m: any) =>
-        m.role === 'assistant'
-          ? new AIMessage(m.content)
-          : new HumanMessage(m.content)
-      ),
-      [handlers]
-    );
-  
   return new StreamingTextResponse(stream);
 }
