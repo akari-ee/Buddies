be16cd154f98363396b7f56fb727b5867e43a33f
lunarmoon7 <49crehbgr@gmail.com>
2023-11-24 20:59:10 +0900

    refactor: 자주 사용하는 값 상수화

---
+++ app/_constant/constant.ts
@@ -0,0 +1,34 @@
+import bomi from '/public/bomi_option.png';
+import yermi from '/public/yermi_option.png';
+import gauri from '/public/gauri_option.png';
+import gyeouri from '/public/gyeouri_option.png';
+
+export const charactersDialogOption = [
+  { name: '보미', src: bomi },
+  { name: '여르미', src: yermi },
+  { name: '가으리', src: gauri },
+  { name: '겨우리', src: gyeouri },
+];
+
+export const bg_colors = ['bg-bomi', 'bg-yermi', 'bg-gauri', 'bg-gyeouri'];
+
+export const border_colors = [
+  'border-bomi',
+  'border-yermi',
+  'border-gauri',
+  'border-gyeouri',
+];
+
+export const ring_colors = [
+  'ring-bomi',
+  'ring-yermi',
+  'ring-gauri',
+  'ring-gyeouri',
+];
+
+export const text_colors = [
+  'text-bomi',
+  'text-yermi',
+  'text-gauri',
+  'text-gyeouri',
+];
