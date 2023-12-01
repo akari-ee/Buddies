aa9458db403ca0021ac3b4e137f43aacc5714cee
lunarmoon7 <49crehbgr@gmail.com>
2023-12-01 18:20:42 +0900

    chore: user page의 ssr 변경으로 인한 수정

--- app/user/_components/user/UserInfo.tsx
+++ app/user/_components/user/UserInfo.tsx
@@ -10,6 +10,13 @@ import CharacterSwiper from './CharacterSwiper';
 import Image from 'next/image';
 
 import hotTopicIcon from '/public/hotTopicIcon.svg';
+import dayjs from 'dayjs';
+import bomi from '@/public/bomi_option.png';
+import yermi from '@/public/yermi_option.png';
+import gauri from '@/public/gauri_option.png';
+import gyeouri from '@/public/gyeouri_option.png';
+import { bg_colors } from '@/app/_constant/constant';
+import { cn } from '@/utils/extendClass';
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -17,27 +24,43 @@ type Props = { chatUsageData: any };
 
 export default function UserInfo({ chatUsageData }: Props) {
   const [value, onChange] = useState<Value>(new Date());
-  
+  const [mark, setMark] = useState(['2023-11-28', '2023-11-26', '2023-11-23']); // 프롬프트 사용 날짜 표시 위함
+
   return (
-    <div className='w-full flex flex-col justify-between'>
-      <div className='mb-4 font-roboto'>
-        <Calendar onChange={onChange} value={value} locale='en-GB' />
-      </div>
-      <div className='h-[56px] flex justify-between items-center px-6 border border-white rounded-md shadow-lg mb-20'>
-        <button className='flex gap-1'>
-          <Image src={hotTopicIcon} alt='hotTopicIcon' width={24} height={24} />
-          나의 핫토픽 모아보기
-        </button>
-        <div>
-          <ChevronRightIcon className='w-4 h-4 stroke-2' />
-        </div>
+    <div className='w-full flex flex-col justify-between mx-auto max-w-md'>
+      <div className='mb-16 font-roboto'>
+        <Calendar
+          onChange={onChange}
+          value={value}
+          className='mx-auto w-full text-sm border-b'
+          tileContent={({ date, view }) => {
+            if (mark.find((x) => x === dayjs(date).format('YYYY-MM-DD'))) {
+              return (
+                <div className='flex justify-center items-center absoluteDiv absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full'>
+                  <div className='dot w-[30px] h-[30px] relative overflow-hidden rounded-full'>
+                    <Image
+                      src={yermi}
+                      alt='dd'
+                      layout='fill'
+                      objectFit='cover'
+                      className={cn(
+                        'absolute rounded-full scale-[2.2] -translate-y-1',
+                        bg_colors[1]
+                      )}
+                    />
+                  </div>
+                </div>
+              );
+            }
+          }}
+        />
       </div>
       <div className='flex flex-col gap-1 mb-24 text-[#171717]'>
         <div className=''>
           <p className='font-PyeongChangPeace text-lg font-bold'>Buddy talk</p>
         </div>
         <div className='text-sm'>
-          <p>이번주는 어떤 버디와 가장 대화를 많이 했을까요?</p>
+          <p>이번 달은 어떤 버디와 가장 대화를 많이 했을까요?</p>
         </div>
       </div>
       <div className='flex justify-center'>
