b88d73c832c890c3c2103f742217e4cc0a141a53
lunarmoon7 <49crehbgr@gmail.com>
2023-12-06 17:55:16 +0900

    refactor: 기존 컴포넌트의 style 수정

--- app/user/_components/user/CharacterSwiper.tsx
+++ app/user/_components/user/CharacterSwiper.tsx
@@ -6,6 +6,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/effect-cards';
 import { cn } from '@/utils/extendClass';
 import { charactersForUserSwiper, text_colors } from '@/app/_constant/constant';
+import badge from '@/public/badge.svg';
 
 type Props = {
   chatUsageData: any;
@@ -13,7 +14,7 @@ type Props = {
 
 export default function CharacterSwiper({ chatUsageData }: Props) {
   return (
-    <div className='w-full lg:max-w-xl'>
+    <div className='w-full lg:w-[80%] lg:max-w-xl'>
       <Swiper
         modules={[EffectCards]}
         speed={1000}
@@ -34,10 +35,10 @@ export default function CharacterSwiper({ chatUsageData }: Props) {
         // onSwiper={(swiper) => console.log(swiper)}
       >
         {charactersForUserSwiper.map((character, index) => (
-          <SwiperSlide key={character.name}>
+          <SwiperSlide key={character.name} className='rounded-lg'>
             <div
               key={character.name}
-              className='w-full h-[500px] pt-7 flex flex-col justify-between items-center rounded-lg bg-gradient bg-gradient-to-tr from-white via-cyan-50 to-transparent backdrop-blur-2xl shadow-2xl'
+              className='w-full h-[600px] pt-7 flex flex-col justify-between items-center bg-[white]/90 backdrop-blur-3xl shadow-2xl drop-shadow-2xl'
             >
               <div
                 id='character-image'
@@ -51,36 +52,58 @@ export default function CharacterSwiper({ chatUsageData }: Props) {
                   sizes='(max-width: 640px) 100vw, 640px'
                 />
               </div>
-              <div
-                className={cn(
-                  'font-PyeongChangPeace text-lg bg-white rounded-full shadow-md px-6 py-2'
-                )}
-              >
-                <p className={cn(text_colors[index], 'font-bold font-PyeongChangPeace')}>{character.name}</p>
+              <div className='bg-white rounded-full shadow-md px-6 py-2 relative'>
+                <p
+                  className={cn(
+                    text_colors[index],
+                    'text-lg font-bold font-PyeongChangPeace'
+                  )}
+                >
+                  {character.name}
+                </p>
+                <Image
+                  src={badge}
+                  alt='badge'
+                  width={22}
+                  height={22}
+                  className='absolute -top-[5px] -right-[6px]'
+                />
               </div>
               <div
                 id='usage-info'
-                className='flex justify-center items-center gap-10 text-sm'
+                className='flex flex-col gap-2 text-sm min-w-full px-6'
               >
-                <div className='flex flex-col justify-center items-center'>
-                  <p>총 대화일</p>
-                  <p className='font-bold'>6일</p>
+                <div className='flex w-full'>
+                  {/* for icon */}
+                  <div id='icon'></div>
+                  <div className='text-[13px] text-[#999]'>총 대화수</div>
+                  <div className='font-bold grow text-right text-sm'>
+                    3134개
+                  </div>
                 </div>
-                <div className='flex flex-col justify-center items-center'>
-                  <p>총 대화시간</p>
-                  <p className='font-bold'>84시간</p>
+                <div className='flex w-full'>
+                  <div id='icon'></div>
+                  <div className='relative text-[13px] text-[#999] after:content-[">"] after:absolute after:-right-3'>
+                    긍정단어
+                  </div>
+                  <div className='font-bold grow text-right text-sm'>84개</div>
                 </div>
-                <div className='flex flex-col justify-center items-center'>
-                  <p>총 대화수</p>
-                  <p className='font-bold'>{chatUsageData[character.name]}</p>
+                <div className='flex w-full'>
+                  <div id='icon'></div>
+                  <div className='relative text-[13px] text-[#999] after:content-[">"] after:absolute after:-right-3'>
+                    부정단어
+                  </div>
+                  <div className='font-bold grow text-right text-sm'>
+                    3050개
+                  </div>
                 </div>
               </div>
-              <div className='text-sm flex justify-center px-7 text-center'>
-                이번달은 해결책 마스터 가을이의 솔로몬 같은 입장 판단을 가장
-                많이 필요로 했어요.
+              <div className='h-[1px] border-[1px] border-dashed border-[#999] w-full opacity-[0.3] mt-[31px]'></div>
+              <div className='text-sm flex justify-center px-14 text-center pt-[23px] pb-[40px] text-[#444]'>
+                {character.msg}
               </div>
-              <div className='w-full h-12 flex-2 flex justify-center place-self-stretch bg-white'>
-                <button className='w-full  text-gray-500 text-sm'>
+              <div className='w-full h-12 flex-2 flex justify-center place-self-stretch bg-white py-[15px]'>
+                <button className='w-full text-[#868686] text-sm'>
                   대화기록 보러가기
                 </button>
               </div>
